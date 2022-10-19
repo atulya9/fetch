775c109b89a104c39d220963fcde8a7b9ff8e13a
atulya9 <tibrewalatulya@gmail.com>
2022-10-19 15:20:46 +0530

    Added error handling for links

--- fetch.py
+++ fetch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Purpose: Fetch the webpage and save it to a file
 
-import requests, argparse
+import requests, argparse, re
 
 def get_args():
     '''
@@ -22,10 +22,21 @@ def main():
     urls = args.links
 
     for item in urls:
-        basedomain = item.split('/')[2]
-        r = requests.get(url=item)
-        with open(f'{basedomain}.html', 'w') as f:
-            f.write(r.text)
+        try:
+            r = requests.get(item)
+            url_pattern = re.search('https?://([A-Za-z_0-9.-]+).*', item)                               # To store the base domain of the URL
+            basedomain = url_pattern.group(1)
+            response = r.text.lower()
+
+            if '<!doctype html>' in response:                                                           # To check if the response is in HTML format
+                with open(f'{basedomain}.html', 'w') as f:
+                    f.write(r.text)
+            else:
+                raise Exception(f'Response for URL: "{item}" is in a different format than HTML')       # Exception handling for any format other than HTML
+        except Exception as e:                                                                          # Exception handling for any other errors
+            print(e)
+
+
 
 if __name__ == '__main__':
     main()
